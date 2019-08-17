ae2d603cbe8e9796778d43c608abddb54a899163
SH-H-B <shiva19173@gmail.com>
2019-08-17 23:26:47 +0100

    Added Error Handlers

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+//const cors = require('cors');
+const {
+  handle400,
+  handle404,
+  handle500,
+  handle422,
+  handle405
+} = require("./errors/errorHandler");
 
 app.use(bodyParser.json());
+//app.use(cors());
+
+app.use("/api", apiRouter);
+
+app.all("/*", (req, res, next) => {
+  next({ status: 404, msg: "Page not found" });
+});
+app.use(handle400);
+app.use(handle404);
+app.use(handle422);
+app.use(handle405);
+app.use(handle500);
 
 module.exports = app;
