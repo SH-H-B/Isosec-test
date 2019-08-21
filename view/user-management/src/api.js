2f7eb4693d3766869bc5fab3c97a364f08acb15f
SH-H-B <shiva19173@gmail.com>
2019-08-21 11:39:27 +0100

    Create api.js send request to get users,user,friends

--- view/user-management/src/api.js
+++ view/user-management/src/api.js
@@ -5,8 +5,21 @@ const url = "http://localhost:9090/api";
 export const getUsers = query => {
   return Axios.get(`${url}/users`, { params: query }).then(
     ({ data: { users } }) => {
-      console.log(users);
       return users;
+    }
+  );
+};
+
+export const getUserById = id => {
+  return Axios.get(`${url}/users/${id}`).then(({ data: { user } }) => {
+    return user;
+  });
+};
+
+export const getUserFriends = id => {
+  return Axios.get(`${url}/users/${id}/friends`).then(
+    ({ data: { friends } }) => {
+      return friends;
     }
   );
 };
