06d290089b0c7b6d6c8bdbe7d5b935f38f371a30
SH-H-B <shiva19173@gmail.com>
2019-08-21 11:40:46 +0100

    Install reach router

--- router/users-router.js
+++ router/users-router.js
@@ -1,7 +1,13 @@
 const usersRouter = require("express").Router();
-const { getAllUsers } = require("../controllers/users-controller");
+const {
+  getAllUsers,
+  getUserById,
+  getUserFriends
+} = require("../controllers/users-controller");
 //const { handle405 } = require("../errors/errorHandler");
 
 usersRouter.route("/").get(getAllUsers);
+usersRouter.route("/:id").get(getUserById);
+usersRouter.route("/:id/friends").get(getUserFriends);
 
 module.exports = usersRouter;
