be5960937635602ee3061656edbf130ee4e48291
SH-H-B <shiva19173@gmail.com>
2019-08-21 12:48:20 +0100

    component Error

--- view/user-management/src/App.js
+++ view/user-management/src/App.js
@@ -3,6 +3,7 @@ import { Router } from "@reach/router";
 import Header from "./components/Header";
 import Users from "./components/users/Users.jsx";
 import User from "./components/User.jsx";
+import Errors from "./components/Errors";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Users path="/" />
         <Users path="/users" />
         <User path="/users/:id" />
+        <Errors path="/*" />
+        <Errors path="/error" />
       </Router>
     </div>
   );
